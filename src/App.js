5d6f8a3492af0e0eeea85cac1ff4830dbafc3089
Lauren <lauren.c4231@gmail.com>
2020-05-28 20:08:53 +0100

    Add TODO for UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.css';
 
+/*--------------------------------------------------------------
+## UI - TODO
+--------------------------------------------------------------*/
+// User should see a textarea box to type in
+// User should see a h4 element displaying the remaining time
+// User should see a button to be able to click on and start the game
+// User should see a display of the word count
+
 function App() {
     return <h1>Speed Typing Game</h1>;
 }
